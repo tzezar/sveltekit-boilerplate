6159b73ec66aecb6e00cba24ad4e6f2d4f4b3538
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-04-23 04:45:23 +0200

    add xs button variant

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -16,6 +16,7 @@ const buttonVariants = tv({
 		},
 		size: {
 			default: "h-10 px-4 py-2",
+			xs: 'h-auto py-0 my-0 px-2 py-1',
 			sm: "h-9 rounded-md px-3",
 			lg: "h-11 rounded-md px-8",
 			icon: "h-10 w-10",
